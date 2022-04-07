f78058147f5a5fac2d7c919ed5dbef9a065aeb80
rockn1989 <gold_100@bk.ru>
2022-04-07 18:27:36 +0300

    верстка шапки

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,39 @@
 $(function () {
-	console.log('App start');
+
+	UIkit.util.on($('.dropdown-menu'), 'beforeshow', function() {
+    $("body").addClass("menu-open");
+  });
+
+  UIkit.util.on($(".dropdown-menu"), "hide", function () {
+    $("body").removeClass("menu-open");
+  });
+
+  UIkit.util.on('#offcanvas-menu-mob', 'show', function () {
+      $("body").addClass("menu-open");
+  });
+
+  UIkit.util.on('#offcanvas-menu-mob', 'hide', function () {
+      $('.js__toggle-icon').removeClass("active");
+      $("body").removeClass("menu-open");
+  });
+
+  $(".js__toggle-icon").on("click", function (e) {
+    e.preventDefault();
+
+    if ($('#offcanvas-menu-mob').hasClass('uk-open')) {
+      $(this).addClass("active");
+    }
+    
+  });
+
+  /**
+   * Mobile search form
+   */
+  var $mobileForm = $(".mobile-search-form");
+
+  $(".js__toggle-mobile-search").on("click", function (e) {
+    e.preventDefault();
+    $mobileForm.slideToggle("350").find("input").focus();
+  });
+
 });
