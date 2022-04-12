e2c3e847e61487380ef595cfa3ee54c595c9de62
rockn1989 <gold_100@bk.ru>
2022-04-12 18:14:06 +0300

    верстка главной 2

---
+++ assets/js/stocks-slider.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+
+  const $heroSlider = $('.stocks-slider');
+
+    $heroSlider.slick({
+      arrows: true,
+      dots: false,
+      infinity: true,
+      fade: true,
+      cssEase: "linear",
+      autoplay: false,
+      autoplaySpeed: 2000,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<div class="btn-slide slick-prev"></div>',
+      nextArrow: '<div class="btn-slide slick-next"></div>',
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+            arrows: true,
+          },
+        },
+      ],
+    });
+
+})();
